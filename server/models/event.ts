8c0c8b4cc32fedc8b31ad101327184023479a0d5
Unknown <allocompte@hotmail.fr>
2020-03-02 04:29:30 +0100

    Change event model
    Events now have a maximum of participants
    beginning < end constraint is now checked at creation time

--- server/models/event.ts
+++ server/models/event.ts
@@ -1,4 +1,14 @@
-import {Table, Column, DataType, Model, ForeignKey, BelongsToMany, BelongsTo} from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  DataType,
+  Model,
+  ForeignKey,
+  BelongsToMany,
+  BelongsTo,
+  Validate,
+  AfterValidate
+} from 'sequelize-typescript';
 import {Organization} from './organization';
 import {User} from './user';
 import {EventParticipation} from './event-participation';
@@ -7,6 +17,8 @@ import {EventParticipation} from './event-participation';
   timestamps: false,
 })
 export class Event extends Model<Event> {
+
+  static DATES_ERROR: Error = new Error('Beginning date must be before end date');
 
   @Column(DataType.STRING)
   name: string;
@@ -20,6 +32,12 @@ export class Event extends Model<Event> {
   @Column(DataType.DATE)
   end: Date;
 
+  @Validate({
+    min: 1
+  })
+  @Column(DataType.INTEGER)
+  maxParticipants: number;
+
   @ForeignKey(() => Organization)
   @Column(DataType.NUMBER)
   organizationId: number;
@@ -29,5 +47,13 @@ export class Event extends Model<Event> {
 
   @BelongsToMany(() => User, () => EventParticipation)
   participants: User[];
+
+  @AfterValidate
+  static checkDatesOrder(instance: Event): Promise<void> {
+    if (instance.beginning.getTime() < instance.end.getTime()) {
+      return Promise.resolve();
+    }
+    return Promise.reject(this.DATES_ERROR);
+  }
 
 }
